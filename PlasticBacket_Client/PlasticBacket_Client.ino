3c43ddbc7950a5d43795d6b8897fc6e1d57a9969
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-12-09 21:39:53 +0700

    Update Comment for Read
    Co-Authored-By: it62070123 <it62070123@users.noreply.github.com>
    Co-Authored-By: it62070200 <it62070200@users.noreply.github.com>
    Co-Authored-By: OHMME <ohmme@users.noreply.github.com>
    Co-Authored-By: phupa pookphanich <thegoz2@users.noreply.github.com>

--- PlasticBacket_Client/PlasticBacket_Client.ino
+++ PlasticBacket_Client/PlasticBacket_Client.ino
@@ -1,38 +1,57 @@
 #include<ESP8266WiFi.h>
 #include<FirebaseArduino.h>
 
+//User กับ Pass ของ WIFI
 const char* user = "Nest";
 const char* pass = "0819296842";
+
+//Host ของ Firebase กับ รหัสยืนยันตัวของ Firebase
 #define Firebase_HOST "plasticbucket-cb721.firebaseio.com"
 #define Firebase_AUT "wZHkaLYqMN3nTecN0t33Pe8HsANOwk2YYI5jFGig"
 
 
 int checker = 50;
 
-void setup() {
-  Serial.begin(9600);
+// Connect to WIFI
+void connect(){
+
+  // set user and pass ของ WIFI
   WiFi.begin(user, pass);
 
+  //ถ้าเชื่อมไม่ติดจะทำการ show '.' ไปจนกว่าจะเชื่อมติด
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(1000);
   }
-  
+
+  //เชื่อมติด Show ข้อความ 'Connected' และ Show IP
   Serial.println("Connected");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+}
 
+//ตั้งค่าต่างๆของ NODEMCU WIFI มีการเรียกการ connect
+void setup() {
+  Serial.begin(9600);
+  connect();
   Firebase.begin(Firebase_HOST, Firebase_AUT);
 }
 
+// ส่วนตรงนี้จะเป็นการทำงานซ้ำๆเรื่อยๆ
 void loop() {
+  //ตั้งค่า path และ id ที่อยู่บน firebase
   String ID = "ID_01/Value";
   String path = "/NodeMCU/" + ID;
+  
+  // อ่านค่าจาก sensor มาเก็บไว้ใน sensor_value
   int sensor_value = analogRead(A0);
 
-    Firebase.setInt(path, sensor_value);
-    Serial.println(sensor_value);
-    delay(5000);
+  // โยนค่าขึ้นไปบน Firebase ด้วยคำสั่ง Firebase.setInt(ที่อยู่, ค่า);
+  Firebase.setInt(path, sensor_value);
+
+  // Show value ผ่านทาง Serial Monitor
+  Serial.println(sensor_value);
+  delay(5000);
   
   
   
