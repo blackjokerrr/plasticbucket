d0fc6f603a1ccb224c9c07e431af8e4850bb616d
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-12-09 22:15:28 +0700

    Delete Client_Server and Rename of Plastic Bucket and Change of Name Variable
    Co-Authored-By: it62070123 <it62070123@users.noreply.github.com>
    Co-Authored-By: it62070200 <it62070200@users.noreply.github.com>
    Co-Authored-By: OHMME <ohmme@users.noreply.github.com>
    Co-Authored-By: phupa pookphanich <thegoz2@users.noreply.github.com>

--- PlayVoice/index.php
+++ PlayVoice/index.php
@@ -9,14 +9,16 @@
 <body>
     <?php
         $url = 'https://plasticbucket-cb721.firebaseio.com/NodeMCU.json'; #ดึงค่าผ่าน FireBase เป็น Json 
-        $response = file_get_contents($url);
-        $path = json_decode($response); #Decode Json ออกเพื่อนำมาใช้
+        $content = file_get_contents($url);
+        $path = json_decode($content); #Decode Json ออกเพื่อนำมาเก็บไว้ในตัวแปร path
 
-        #Check Plastic Voice ว่าค่าเข้ามาหรือเปล่า
-        foreach($path as $i){
-            if($i->Value >= 50 && $i->Value != 1024){
-                print '<audio autoplay>'.'<source src="plasticvoice.mp3" type="audio/mpeg">'.'</audio>';
-                print "<p>Value: ".$i->Value." Name: ".$i->Name."</p>";
+        #Check Plastic Voice ว่าค่าเข้ามาหรือเปล่าโดยการวน Loop เข้าไปใน Json ของ API Firebase ของ Project
+        foreach($path as $navigate){
+            /* เงื่อน Check ค่าที่ส่งขึ้นมาว่า มีค่าตั้งแต่ 50 และ ไม่เท่ากับ 1024 หรือเปล่า เพราะ ค่า <= 500 คือค่าที่ Laser อ่อนกำลังลง คือมีวัตถุมาบัง Laser ถ้า Laser มีกำลังมากจะ < 1024 ลงไปไม่มาก
+            เพราะ Max ที่สุดที่ Sensor สามารถวัดได้คือ 1024 ซึ่ง เราจะยิง Laser ตลอดเวลาจึงมีเงื่อนไขว่าจะต้องไม่เท่ากับ 1024 */
+            if($navigate->Value <= 500 && $navigate->Value != 1024){
+                print '<audio autoplay>'.'<source src="plasticvoice.mp3" type="audio/mpeg">'.'</audio>'; # เล่นเสียงจาก File เสียง ซึ่งอัดมาจาก Google Translate
+                print "<p>Value: ".$navigate->Value." Name: ".$navigate->Name."</p>"; #Show ค่าและ ID ของ NodeMCU ที่ส่งค่าขึ้นมาบน Firebase
             }
         }
 
