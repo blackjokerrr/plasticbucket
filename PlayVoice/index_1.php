b47772dd919366409defdd0f332b39969c549d17
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-12-15 23:31:14 +0700

    เปลี่ยนชื่อ index เพื่อ check webhosting

--- PlayVoice/index_1.php
+++ PlayVoice/index_1.php
@@ -44,7 +44,7 @@
         print '<div class="container">';
         #Check Plastic Voice ว่าค่าเข้ามาหรือเปล่าโดยการวน Loop เข้าไปใน Json ของ API Firebase ของ Project
         foreach($path as $navigate){
-            /* เงื่อน Check ค่าที่ส่งขึ้นมาว่า มีค่าตั้งแต่ 50 และ ไม่เท่ากับ 1024 หรือเปล่า เพราะ ค่า <= 500 คือค่าที่ Laser อ่อนกำลังลง คือมีวัตถุมาบัง Laser ถ้า Laser มีกำลังมากจะ < 1024 ลงไปไม่มาก
+            /* เป็นเงื่อนไขในการ Check ค่าที่ส่งขึ้นมาว่า มีค่าตั้งแต่ 50 และ ไม่เท่ากับ 1024 หรือเปล่า เพราะ ค่า <= 500 คือค่าที่ Laser อ่อนกำลังลง คือมีวัตถุมาบัง Laser ถ้า Laser มีกำลังมากจะ < 1024 ลงไปไม่มาก
             เพราะ Max ที่สุดที่ Sensor สามารถวัดได้คือ 1024 ซึ่ง เราจะยิง Laser ตลอดเวลาจึงมีเงื่อนไขว่าจะต้องไม่เท่ากับ 1024 */
             if($navigate->Value <= 500 && $navigate->Value != 1024){
                 #print '<audio autoplay>'.'<source src="plasticvoice.mp3" type="audio/mpeg">'.'</audio>'; # เล่นเสียงจาก File เสียง ซึ่งอัดมาจาก Google Translate
